499459851903a8f073a01cadfe9094a9fef35f08
Richard Delgado <rdelgado134@gmail.com>
2024-01-22 17:17:17 -0800

    Passing the first test, finish at another time.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToBeRemoved) 
+{
+    let size = array.length;
+    let result = [];
 
+    for(let i = 0; i < size; i++)
+    {
+        if(array[i] != elementToBeRemoved) 
+        {
+            result.push(array[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
