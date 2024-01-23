660c47b022136b01c3908b1ee27d6ac610568bd8
Richard Delgado <rdelgado134@gmail.com>
2024-01-22 16:17:15 -0800

    Tests all passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, timesRepeated) {
 
+    if(timesRepeated < 0) { return "ERROR"}
+    let result = "";
+    
+    for(let i = 0; i < timesRepeated; i++)
+    {
+        result += word;
+    }
+    return result
 };
 
 // Do not edit below this line
