b20b0f33893691b8be035c02049b76ddd9aa570e
rdelgado134 <rdelgado134@gmail.com>
2024-02-01 15:52:59 -0800

    Code adds the sum of two given number values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) 
+{
+    if(startNum < 0 || endNum < 0 || typeof startNum === 'string' || typeof endNum === 'string')
+    {return "ERROR";}
+   /* if(typeof startNum === 'number' && Number.isInteger(startNum)) {return "ERROR";}
+    if(typeof endNum === 'number' && Number.isInteger(endNum)) {return "ERROR"}*/
 
+    let result = 0;
+
+    if(startNum < endNum)
+    {
+        for(let i = startNum; i <= endNum; i++)
+        {
+            result += i;
+        }
+    }
+    else if (startNum > endNum)
+    {
+        for(let j = endNum; j <= startNum; j++)
+        {
+            result +=j;
+        }
+    }
+    if(result == 0) {return "ERROR"}
+    return result;
 };
 
 // Do not edit below this line
