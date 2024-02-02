f8ecfb1c4977f72076bb90db9166693d209f4171
rdelgado134 <rdelgado134@gmail.com>
2024-02-01 16:35:05 -0800

    Code determines if a given year is leap year or not.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) 
+{
+    //needs to be divisible by 4 
+    // cannot be divisible by 100 but can be divisible by 400
+    let leapYear = false;
 
+    if(year % 4 == 0)
+    {
+        leapYear = true;
+    }
+
+    if(year % 100 == 0)
+    {
+        leapYear = false;
+        if(year % 400 == 0)
+        {
+            leapYear = true;
+        }
+    }
+
+    return leapYear;
 };
 
 // Do not edit below this line
