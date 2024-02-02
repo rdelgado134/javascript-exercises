2ff6a68aa3706c88e76a04ac8f9c3aa56558a81a
rdelgado134 <rdelgado134@gmail.com>
2024-02-01 17:48:33 -0800

    Wrote two functions, one converting celsius to fahrenheit and vice versa.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) 
+{
+  let num = (5/9) * (tempF -32);
+  return Math.round(num * 10) /10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) 
+{
+  let num = (tempC) * (9/5) +32;
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
