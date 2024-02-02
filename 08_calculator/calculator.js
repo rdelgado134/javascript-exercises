6120db5748825ad6cd39b2016529d97ace1a2d21
rdelgado134 <rdelgado134@gmail.com>
2024-02-01 18:13:45 -0800

    Wrote all necessary functions for odin calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(num1, num2) 
+{
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) 
+{
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) 
+{
+  let result = 0;
+	for(let i = 0; i < nums.length; i++)
+  {
+    result +=nums[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) 
+{
+  result = 1;
+	for(let i = 0; i < nums.length; i++)
+  {
+    result *=nums[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, power) 
+{
+  let result = num1;
+	for(let i = 2; i <= power; i++)
+  {
+    result *= num1;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) 
+{
+ //5! = 5x4x3x2x1 == 120	
+ let result = num;
+ for(let i = num -1; i > 0; i--)
+ {
+    result *= i;
+ }
+
+ if(num == 0) {result = 1;}
+ return result;
 };
 
 // Do not edit below this line
