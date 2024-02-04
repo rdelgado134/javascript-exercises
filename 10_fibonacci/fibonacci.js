0d81a395e2464fbe0bc78beec120770d328ba4be
rdelgado134 <rdelgado134@gmail.com>
2024-02-04 14:44:54 -0800

    Wrote fibonnacci method, all tests passed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) 
+{
+    //Fn = Fn-1 + Fn-2
+    if(n === 0){return 0;}
+    if(n < 0) {return "OOPS";}
+    let fib = [0, 1];
+        for (let i = 2; i <= n; i++) 
+        {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+     return fib[n];
 };
 
 // Do not edit below this line
