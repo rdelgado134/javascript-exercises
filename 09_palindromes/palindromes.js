74b03995b2cdfc0dd785db07fef806baa3eaca1c
rdelgado134 <rdelgado134@gmail.com>
2024-02-03 21:39:15 -0800

    All test cases pass.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+function isPunctuation(char) {
+    // Define a regular expression for punctuation characters
+    const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/;
+  
+    // Test if the given character matches the punctuation pattern
+    return punctuationRegex.test(char);
+  }
 
+const palindromes = function (str) 
+{
+      // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  // Compare the original and reversed strings
+  return cleanStr === cleanStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
